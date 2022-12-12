69955621c9a2866f87f1a688e9c33ad84518d82e
Bob Vandevliet <bob.vandevliet@nl.toyota-industries.eu>
2022-12-12 20:04:52 +0100

    small cherry-picks

--- src/renderer/includes/functions-html.ts
+++ src/renderer/includes/functions-html.ts
@@ -7,3 +7,11 @@
  */
 export const getTemplateClone = (id: string) =>
   (document.getElementById(id) as HTMLTemplateElement)?.content?.cloneNode(true);
+
+/**
+ * Get the position index of a given element relative to its sibling elements.
+ *
+ * @param elem An element to find the index position of.
+ */
+export const getIndex = (elem: HTMLElement) =>
+  [...elem.parentNode.children].indexOf(elem);
